b8083dcbf799475f94b7000bae736fa15deefde7
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-11 12:37:53 -0500

    new script

---
+++ git-pull.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+odir=${CHESS_DIR:-$PWD}
+echo "CHESS directory: $odir"
+cd $odir
+
+# processes
+for srv in golib Authz MetaData DataDiscovery DataManagement DataBookkeeping Frontend gotools/client
+do
+    echo
+    echo "### visit $srv service..."
+    echo "--- git pull"
+    cd $srv
+    git pull
+    cd - 2>&1 1>& /dev/null
+done
