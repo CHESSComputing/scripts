af7707666dc5cf836bc1f21f01f6bbfa30c887c8
Valentin Kuznetsov <vkuznet@gmail.com>
2024-01-29 16:32:05 -0500

    new script

---
+++ git-push-tags.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+odir=${CHESS_DIR:-$PWD}
+echo "CHESS directory: $odir"
+cd $odir
+
+# processes
+services="Authz MetaData DataDiscovery DataManagement DataBookkeeping Frontend SpecScansService MLHub gotools/client"
+for srv in $services
+do
+    echo
+    echo "### visit $srv service..."
+    cd $srv
+    git push --tags
+    cd - 2>&1 1>& /dev/null
+done
