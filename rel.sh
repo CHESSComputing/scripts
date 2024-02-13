713cd29f0379348fde903abc5e81e28f9c2133f7
Valentin Kuznetsov <vkuznet@gmail.com>
2024-02-13 12:53:02 -0500

    Add rel.sh tool to view latest releases

---
+++ rel.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cdir=${CHESSWORK_DIR:-$PWD}
+echo "CHESSWORK directory: $cdir"
+cd $cdir
+
+# processes
+services="Authz MetaData DataDiscovery DataManagement DataBookkeeping Frontend SpecScansService MLHub PublicationService gotools/foxden"
+for srv in $services
+do
+    echo
+    echo "### $srv service..."
+    cd $srv
+    gh release list | grep -v Draft | head -1
+    code=$?
+    if [ $code -ne 0 ]; then
+        exit $code
+    fi
+    cd - 2>&1 1>& /dev/null
+done
