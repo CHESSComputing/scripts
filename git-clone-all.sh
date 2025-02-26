972f87dbd38a9d03b3ee0fd9012e9e1e34980049
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-26 08:39:24 -0500

    bug fixes

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -4,11 +4,10 @@ odir=${CHESS_DIR:-$PWD}
 echo "CHESS directory: $odir"
 cd $odir
 
-# processes
-servcies="gotools Auth DataBookkeeping DataDiscovery DataManagement FOXDEN Frontend Kubernetes MLHub MetaData PublicationService SpecScansService"
+services="gotools Authz DataBookkeeping DataDiscovery DataManagement FOXDEN Frontend Kubernetes MLHub MetaData PublicationService SpecScansService"
 for srv in $services
 do
     echo
     echo "### clone $srv ..."
-    git clone git@github.com:CHESSComputing/${srv}.git
+    git clone https://github.com/CHESSComputing/${srv}.git
 done
