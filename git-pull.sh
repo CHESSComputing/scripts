8254b12f5c567dc9b666d2046c54deae181ed400
Valentin Kuznetsov <vkuznet@gmail.com>
2025-02-27 15:39:19 -0500

    add golib to pull

--- git-pull.sh
+++ git-pull.sh
@@ -5,7 +5,7 @@ echo "CHESS directory: $odir"
 cd $odir
 
 # processes
-services="Authz MetaData DataDiscovery DataManagement DataBookkeeping Frontend SpecScansService MLHub PublicationService gotools/foxden gotools/migrate gotools/transform"
+services="golib Authz MetaData DataDiscovery DataManagement DataBookkeeping Frontend SpecScansService MLHub PublicationService gotools/foxden gotools/migrate gotools/transform"
 for srv in $services
 do
     echo
